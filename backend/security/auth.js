314a7c915de0831d7c551a810ae890116d2f8ef8
eman1989-dev <emanuelfallas2@gmail.com>
2026-04-27 23:28:43 -0600

    Generar token

---
+++ backend/security/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+function generarToken(usuario) {
+    return jwt.sign({id: usuario._id, email: usuario.email}, process.env.JWT_SECRET, {expiresIn: '1h'});
+}
+
+function verificarToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) return res.status(401).json({message: 'Token no proporcionado'});
+
+    const token = authHeader.split(' ')[1];
+    if (!token) return res.status(401).json({message: 'Token no proporcionado'});
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.usuarioId = decoded.id;
+        next();
+    } catch (err) {
+        return res.status(401).json({message: 'Token no válido'});
+    }
+}
+
+module.exports = {verificarToken, generarToken};
